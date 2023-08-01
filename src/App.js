2130c3abd0f803a6817354adfabb96f5c5b08d76
madijha <adityakumarjha216@gmail.com>
2023-08-01 20:19:12 +0530

    newest commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
+  let [count,setCount]=useState(0);
+  const incButton=()=>{
+    setCount(++count)
+  }
+  const decButtonButton=()=>{
+    setCount(--count)
+  }
+
   return (
     <div className="App">
-      TESTING
+      <p>{count}</p>
+      <button onClick={incButton}>Increment</button>
+      <button onClick={decButton}>Decrement</button>
     </div>
   );
 }
